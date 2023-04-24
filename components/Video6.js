58cee805ae91299ea8b8f0f135c3e4470a03b873
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-04-24 15:18:34 +0100

    video edit

--- components/Video6.js
+++ components/Video6.js
@@ -23,15 +23,15 @@ const Video6 = () => {
       <Video
         ref={video}
         style={styles.video}
-        source={{uri:'https://emekaagara.com/wp-content/uploads/2023/04/pexels-cottonbro-studio-4008365-1080x2048-50fps.mp4'}}
-        // source={require('../assets/video1.mp4')}
+        // source={{uri:'https://emekaagara.com/wp-content/uploads/2023/04/pexels-cottonbro-studio-4008365-1080x2048-50fps.mp4'}}
+        source={require('../assets/video1.mp4')}
         shouldPlay
         resizeMode='cover'
         isLooping
         onPlaybackStatusUpdate={setStatus}
       />
 
-      <Text style={styles.mainText}>wRefined Shopping experience</Text>
+      <Text style={styles.mainText}>Refined Shopping experience</Text>
       <Text style={styles.subText}>Refined Shopping experience lorem ipsum lorem ipsum lorem ipsumlorem ipsum</Text>
 
       <TouchableOpacity onPress={onNextPress} style={styles.ButtonContainer}>
