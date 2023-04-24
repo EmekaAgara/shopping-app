3fd158fa90e26e7b37285fdf3e5f8e147771d23d
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-04-24 17:48:57 +0100

    product routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
-const express = require ('express');
+const express = require('express');
+const productRoutes = require('./router/productRoutes');
+
 const app = express();
 const PORT = 3000;
+
+app.use('/products', productRoutes);
 
 app.get('/', (req, res) => {
     res.send('<h2>Hello world</h2>');
 });
-
 
 
 app.listen(PORT, () => {
