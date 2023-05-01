17c3314dac3abb838efcce19a2455f04f3e7f378
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-05-01 17:57:43 +0100

    square api integration fix

--- screens/Pay.js
+++ screens/Pay.js
@@ -1,87 +1,33 @@
-import { StyleSheet, Text, View,Image,TouchableOpacity, SafeAreaView, useWindowDimensions } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
-import CustomInput from '../components/CustomInput'
-import CustomButton from '../components/CustomButton'
-// import { SQIPCardEntry } from 'react-native-square-in-app-payments'
-
-const Pay = () => {
-
-  const {height} = useWindowDimensions();
-
-    const navigation = useNavigation();
-
-    const onSignupPressed = () => {
-      navigation.navigate('Signup');
-    };
-
-    const onLoginPressed = () => {
-      navigation.navigate('ProductsScreen');
-    };
+import React, { PureComponent } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from "react-native";
+import { WebView } from 'react-native-webview';
 
 
-  return (
-
-
-    <SafeAreaView style={styles.container}>
-      <View style={styles.root}>
-        <Image
-          source={require('../assets/XRP.png')}
-          style={[styles.logo, {height:height * 0.2}]}
+export default class Pay extends PureComponent {
+  render() {
+    
+    return (
+      <SafeAreaView
+        style={styles.safeArea}>
+        <WebView 
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+          source={{ uri: 'https://xrpl-project.netlify.app/create-trustline-send-currency-ow' }}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
         />
-        <Text style={styles.title}>Login to your account</Text>
-        <CustomInput name="email" placeholder="Email address" rules={{required:'Enter your email address'}}/>
-        <CustomInput name="password" placeholder="Password" secureTextEntry rules={{required:'Enter your password',}} />
-        <CustomButton text="Login" onPress={onLoginPressed}/>
-        <CustomButton text="Have an account? Sign up" onPress={onSignupPressed} type="tertiary"/>
-      </View>
-    </SafeAreaView>
-  )
+      </SafeAreaView>
+    );
+  }
 }
 
-export default Pay
-
 const styles = StyleSheet.create({
-
-  container: {
-    backgroundColor:'brown'
-  },
-
-
-  root: {
-    alignItems:'center',
-    padding:20,
-    paddingTop:'40%',
-    backgroundColor:'brown',
-    height:'100%',
-  },
-
-  logo:{
-    width:'70%',
-    maxWidth:150,
-    maxHeight:150,
-    borderRadius:10,
-    resizeMode:'cover',
-    alignSelf:'flex-start',
-  },
-
-  title:{
-    fontSize:24,
-    fontWeight:'bold',
-    color:'black',
-    margin:10,
-    alignSelf:'flex-start',
-  },
-
-  text:{
-    color:'gray',
-    marginVertical:10,
-
-  },
-
-  link:{
-    color:'#4765A9'
-
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#000',
+    maxWidth:'100%',
+    // max-width: '100%',
+    // overflow-x: hidden,
   }
-
-});
+})
