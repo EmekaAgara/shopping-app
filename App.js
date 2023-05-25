2453c74acd15f011c4def03157c0b86c12f17d9d
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-05-25 12:22:59 +0100

    ai products prompt updated

--- App.js
+++ App.js
@@ -34,7 +34,6 @@ export default function App() {
     <NavigationContainer>
       <SafeAreaProvider>
         <Stack.Navigator>
-
         <Stack.Screen
             name='SplashScreen'
             component={SplashScreen}
